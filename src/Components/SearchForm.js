446bb31aa9ec722a634a5e5ab90da2cb760b45b1
Leah <LeahKahwadjian@gmail.com>
2020-06-18 17:34:28 -0400

    set up form, logic to find repo by username

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 class SearchForm extends Component {
   render() {
-    return <p>This is the search form component</p>;
+    return (
+      <StyledForm id="username-form" onSubmit={this.props.onSubmit}>
+        <TextField
+          id="github-user-name"
+          label="Username"
+          value={this.props.searchText}
+          onChange={this.props.handleChange}
+          variant="outlined"
+        />
+
+        <Button className="form-submit" type={"submit"}>
+          Submit
+        </Button>
+        <p>this is search text --- {this.props.searchText}</p>
+        <p>this is submitted text --- {this.props.submittedText}</p>
+      </StyledForm>
+    );
   }
 }
+
+const StyledForm = styled.form``;
 
 export default SearchForm;
